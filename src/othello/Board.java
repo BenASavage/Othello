dc67856a06ffc0cb039efbe4156805ffa10802d2
benja <benjamin.a.savage@gmail.com>
2021-02-03 17:41:19 -0700

    Progress on disc flipping logic

--- src/othello/Board.java
+++ src/othello/Board.java
@@ -39,7 +39,8 @@ public class Board {
     /**
      * Sets the board to its starting state. <p></p>
      * This method creates an 8x8 grid, using a 2d Disc array, and fills the center 4 positions/tiles with Discs of
-     * alternating colors (White, Black). The pattern starts at (3,3) with white and continues in a square.
+     * alternating colors (White, Black) such that each color is diagonal to itself. E.g. WB/BW.
+     * The pattern starts at (3,3) with white.
      * Used for the instantiation of the tiles object in the Board constructor.
      * @return 2d Disc array of size 8x8 with the center four tiles filled with Discs of alternating colors
      * @see #tiles
@@ -49,9 +50,9 @@ public class Board {
     private Disc[][] initTiles() {
         Disc[][] tiles = new Disc[8][8];
         tiles[3][3] = new Disc(Color.WHITE);
+        tiles[4][3] = new Disc(Color.BLACK);
         tiles[3][4] = new Disc(Color.BLACK);
-        tiles[4][3] = new Disc(Color.WHITE);
-        tiles[4][4] = new Disc(Color.BLACK);
+        tiles[4][4] = new Disc(Color.WHITE);
         return tiles;
     }
 
@@ -114,16 +115,73 @@ public class Board {
     public void placeDisc(Coordinate coordinate) {
         Disc placedDisc = new Disc(playerTurn);
         tiles[coordinate.x][coordinate.y] = placedDisc;
+
         //TODO logic for updating the board
         //Checks to the right
-        for (int i = coordinate.x + 1; i < tiles[0].length - coordinate.x - 1; i++) {
-            if (placedDisc.equals(tiles[i][coordinate.y]) && i > coordinate.x + 1) {
-                for (int j = coordinate.x + 1; j < i - 1; j++) {
-                    tiles[j][coordinate.y].switchColor();
+        try {
+            if (tiles[coordinate.x + 1][coordinate.y] != null
+                    && !placedDisc.equals(tiles[coordinate.x + 1][coordinate.y])) {
+                for (int i = coordinate.x + 2; i < tiles[0].length - coordinate.x - 1; i++) {
+                    if (placedDisc.equals(tiles[i][coordinate.y])) {
+                        for (int j = coordinate.x + 1; j < i; j++) {
+                            tiles[j][coordinate.y].switchColor();
+                        }
+                        break;
+                    }
                 }
-                break;
             }
+        } catch (ArrayIndexOutOfBoundsException ignored) {
         }
+
+        //Checks diagonally up and right
+        try {
+            if (tiles[coordinate.x + 1][coordinate.y - 1] != null
+                    && !placedDisc.equals(tiles[coordinate.x + 1][coordinate.y - 1])) {
+                int j = coordinate.y - 2;
+                for (int i = coordinate.x + 2; i < tiles[0].length - coordinate.x - 1 && j >= 0; i++) {
+                    if (placedDisc.equals(tiles[i][j])) {
+                        int h = coordinate.y - 1;
+                        for (int k = coordinate.x + 1; k < i; k++) {
+                            tiles[k][h].switchColor();
+                            h--;
+                        }
+                        break;
+                    }
+                    j--;
+                }
+            }
+        } catch (ArrayIndexOutOfBoundsException ignored) {
+        }
+
+        //Checks upwards
+        try {
+            if (tiles[coordinate.x][coordinate.y - 1] != null
+                    && !placedDisc.equals(tiles[coordinate.x][coordinate.y + 1])) {
+                for (int i = coordinate.y - 2; i > 0; i--) {
+                    if (placedDisc.equals(tiles[coordinate.x][i])) {
+                        for (int j = coordinate.y - 1; j > i; j--) {
+                            tiles[coordinate.x][j].switchColor();
+                        }
+                        break;
+                    }
+                }
+            }
+        } catch (ArrayIndexOutOfBoundsException ignored) {
+        }
+
+        //Checks diagonally up and left
+
+
+        //Checks to the left
+
+
+        //Checks diagonally down and left
+
+
+        //Checks downwards
+
+
+        //Checks diagonally down and to the right
 
         switchTurn();
     }
--- tests/othello/BoardTest.java
+++ tests/othello/BoardTest.java
@@ -23,7 +23,23 @@ class BoardTest {
     @Test
     void placeDiscFlippingRight() {
         Board board = new Board();
-        board.placeDisc(new Coordinate(5,4));
+        board.placeDisc(new Coordinate(2,3));
+        assertEquals(Color.BLACK, board.getTiles()[3][3].getColor());
+    }
+
+    @Test
+    void placeDiscFlippingUpRight() {
+        Board board = new Board();
+        board.placeDisc(new Coordinate(2,3));
+        board.placeDisc(new Coordinate(2,4));
+        board.placeDisc(new Coordinate(2,5));
+        assertEquals(Color.BLACK, board.getTiles()[3][4].getColor());
+    }
+
+    @Test
+    void placeDiscFlippingUp() {
+        Board board = new Board();
+        board.placeDisc(new Coordinate(4,5));
         assertEquals(Color.BLACK, board.getTiles()[4][4].getColor());
     }
 }
