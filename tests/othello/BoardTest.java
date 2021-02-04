fd19b1a8e00a022173ad1297340d986f02745953
benja <benjamin.a.savage@gmail.com>
2021-02-03 17:48:39 -0700

    Testing

--- tests/othello/BoardTest.java
+++ tests/othello/BoardTest.java
@@ -21,6 +21,17 @@ class BoardTest {
     }
 
     @Test
+    void placeDiscAlternateColors() {
+        Board board = new Board();
+        Disc testDiscBlack = new Disc(Color.BLACK);
+        Disc testDiscWhite = new Disc(Color.WHITE);
+        board.placeDisc(new Coordinate(5,4));
+        board.placeDisc(new Coordinate(3, 2));
+        assertEquals(testDiscBlack, board.getTiles()[5][4]);
+        assertEquals(testDiscWhite, board.getTiles()[3][2]);
+    }
+
+    @Test
     void placeDiscFlippingRight() {
         Board board = new Board();
         board.placeDisc(new Coordinate(2,3));
@@ -41,5 +52,17 @@ class BoardTest {
         Board board = new Board();
         board.placeDisc(new Coordinate(4,5));
         assertEquals(Color.BLACK, board.getTiles()[4][4].getColor());
+    }
+
+
+
+    @Test
+    void placeDiscMultipleFlips() {
+        Board board = new Board();
+        board.placeDisc(new Coordinate(2,3));
+        board.placeDisc(new Coordinate(2,4));
+        board.placeDisc(new Coordinate(2,5));
+        assertEquals(Color.BLACK, board.getTiles()[3][4].getColor());
+        assertEquals(Color.BLACK, board.getTiles()[2][4].getColor());
     }
 }
