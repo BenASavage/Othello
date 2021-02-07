6f832aca9b163fecb573b28f0c5746eca8141a49
benja <benjamin.a.savage@gmail.com>
2021-02-06 17:41:56 -0700

    Docs

---
+++ src/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"othello","l":"Board"},{"p":"othello","l":"Coordinate"},{"p":"othello","l":"Disc"},{"p":"othello","l":"GUI"}]
